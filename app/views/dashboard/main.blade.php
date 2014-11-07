7d787cff2ae2e2c3b5efd48a927678a4a0ebd869
ValentinKostyuk <valentin.kostiuk@gmail.com>
2014-11-08 01:34:18 +0200

    Dashboard main page layout changed not to be borring

--- app/views/dashboard/main.blade.php
+++ app/views/dashboard/main.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.dashboardLayout')
 @section('content')
-    <h1>SUPER CONTENT</h1>
-    <h1>SUPER CONTENT</h1>
-    <h1>SUPER CONTENT</h1>
-    <h1>SUPER CONTENT</h1>
-    <h1>SUPER CONTENT</h1>
+    <ul class="dashboard-big-menu-list">
+        <li class="dashboard-big-menu-list-item">
+            <a href="{{{action('CarouselManagementController@carouselItems')}}}"><img src="/statics/images/main/dashboard-carousel.jpg" class="dashboard-big-menu-image"><span>Manage Carousel</span></a>
+        </li>
+        <li class="dashboard-big-menu-list-item">
+            <a href="{{{action('ProductManagementController@productItems')}}}"><img src="/statics/images/main/dashboard-gallery.jpg" class="dashboard-big-menu-image"><span>Manage Gallery</span></a>
+        </li>
+        <li class="dashboard-big-menu-list-item">
+            <a href="{{{action('UserManagementController@users')}}}"><img src="/statics/images/main/dashboard-users.jpg" class="dashboard-big-menu-image"><span>Manage Users</span></a>
+        </li>
+        <li class="dashboard-big-menu-list-item">
+            <a href="#hohoAdminHelp"><img src="/statics/images/main/dashboard-admin.jpg" class="dashboard-big-menu-image"><span>Contact Admin</span></a>
+        </li>
+    </ul>
 @stop
